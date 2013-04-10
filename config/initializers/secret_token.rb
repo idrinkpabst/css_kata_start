b190ff17aeca9f872c338e616ec6cecc3c06f2f1
Jake Parks <jakob.parks@gmail.com>
2013-04-09 19:34:06 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CssKataStart::Application.config.secret_token = 'bcb5c7032b4636bbf0e4f270e0bf64ff37d5fbf7302dbbe9d8b0ccc9ede99b067738a609a788354e046a8f98b1d71f76c8fb284a3e4421aea6d1ae36ebcb9496'
